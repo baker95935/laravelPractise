91c85ba44a3c14c0cb85bd74a27805591d20bf15
baker95935 <baker95935@qq.com>
2016-12-23 16:34:45 +0800

    first commit

---
+++ database/migrations/2016_12_22_085435_create_friends_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFriendsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('friends', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->string('url');
+			$table->string('note')->nullable();
+			$table->integer('order');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('friends');
+	}
+
+}
